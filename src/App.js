cfa54c98f892fedf0e2567749052f494522ebce0
Alexandr <alexandrpo271@gmail.com>
2021-09-14 15:10:56 +0300

    working project version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import './App.scss';
-import {Header} from "./components/Header/Header";
-import {Login} from "./components/LoginPage/Login";
-import {Register} from "./components/RegisterPage/Register";
-import {TasksPage} from "./components/TasksPage/TasksPage";
-import {BrowserRouter, Redirect, Route} from "react-router-dom";
-import background2 from "./assets/img/background2.jpg"
+import { Header } from './components/Header/Header';
+import { Login } from './components/LoginPage/Login';
+import { Register } from './components/RegisterPage/Register';
+import { TasksPage } from './components/TasksPage/TasksPage';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import background2 from './assets/img/background2.jpg';
 
 export const App = () => {
   return (
-      <BrowserRouter>
-          <div>
-              <img src={background2} alt="#"/>
-              <div className='appBackdrop'>
-                  <div className='appWrapper'>
-                      <Route path='/' exact ><Redirect to='/tasksPage'/></Route>
-                      <Header/>
-                      <Route path='/login' render={() => <Login/>}/>
-                      <Route path='/register' render={() => <Register/>}/>
-                      <Route path='/tasksPage' render={() => <TasksPage/>}/>
-                  </div>
-              </div>
+    <BrowserRouter>
+      <div>
+        <img src={background2} alt="#" />
+        <div className="appBackdrop">
+          <div className="appWrapper">
+            <Route path="/" exact>
+              <Redirect to="/tasksPage" />
+            </Route>
+            <Header />
+            <Route path="/login" render={() => <Login />} />
+            <Route path="/register" render={() => <Register />} />
+            <Route path="/tasksPage" render={() => <TasksPage />} />
           </div>
-      </BrowserRouter>
+        </div>
+      </div>
+    </BrowserRouter>
   );
-}
+};
